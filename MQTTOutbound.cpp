11847fec639f5fe3565c7295435edfe152ab072e
rossbeazley <ross@rossbeazley.co.uk>
2017-10-22 09:21:42 +0100

    Leave some thiking outloud design comments

--- MQTTOutbound.cpp
+++ MQTTOutbound.cpp
@@ -1,6 +1,10 @@
 
 #include "MQTTOutbound.h"
 
+//think this could be implementing a general "publisher" of the MQTT implementation
+//it needs a mediator to observer the app core and publish when required
+//this mediator can probably either be an pub/sub or allow the sub to poke it
+
 MQTTOutbound::MQTTOutbound(PubSubClient & psC, char * mqttTopic) : _mqttClient(psC), _mqttTopic{mqttTopic}
 {
 
