bb253d2b99e7bcf6b9fa2e0017c1fd5f66c58803
beazlr02 <ross.beazley@bbc.co.uk>
2018-01-18 13:14:09 +0000

    Add map for gpio isrs

--- lib/InteruptGPIO/src/InteruptGPIOButton.cpp
+++ lib/InteruptGPIO/src/InteruptGPIOButton.cpp
@@ -1,6 +1,7 @@
 #include "InteruptGPIOButton.h"
 #include "Arduino.h"
 #include <functional>
+#include <map>
 
 std::function<void(uint8_t)> InteruptGPIOButtonISRLambda14;
 
@@ -9,8 +10,18 @@ void InteruptGPIOButtonISR14()
   InteruptGPIOButtonISRLambda14(14);
 }
 
+
+
+
+typedef void (*ISRFunctionPointer)(void);
+std::map<uint8_t, ISRFunctionPointer> isrFunctionsByPinNumber = { {14,InteruptGPIOButtonISR14} };
+
+
+
 InteruptGPIOButton::InteruptGPIOButton(AbstractButton & ab) : button(ab)
 {
+
+
   InteruptGPIOButtonISRLambda14 = [this](uint8_t pinNo)->void{
     if (digitalRead(pinNo))
     {
@@ -23,6 +34,11 @@ InteruptGPIOButton::InteruptGPIOButton(AbstractButton & ab) : button(ab)
 
 
   };
+
+  void (*isrfp)(void) = InteruptGPIOButtonISR14;
+
+  isrfp = isrFunctionsByPinNumber.find(14)->second;
+
   pinMode(14, INPUT_PULLUP);
-  attachInterrupt(14, InteruptGPIOButtonISR14, CHANGE);
+  attachInterrupt(14, isrfp, CHANGE);
 }
